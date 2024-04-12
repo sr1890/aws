4a3c4739b38f9150c531fdde676cceda057a13bc
sr1890 <santosh.ray@hotmail.com>
2024-04-12 11:02:52 +0530

    week2

--- frontend-react-js/src/App.js
+++ frontend-react-js/src/App.js
@@ -1,7 +1,6 @@
 import './App.css';
 
 import HomeFeedPage from './pages/HomeFeedPage';
-import NotificationsFeedPage from './pages/NotificationsFeedPage';
 import UserFeedPage from './pages/UserFeedPage';
 import SignupPage from './pages/SignupPage';
 import SigninPage from './pages/SigninPage';
@@ -10,6 +9,7 @@ import MessageGroupsPage from './pages/MessageGroupsPage';
 import MessageGroupPage from './pages/MessageGroupPage';
 import ConfirmationPage from './pages/ConfirmationPage';
 import React from 'react';
+import process from 'process';
 import {
   createBrowserRouter,
   RouterProvider
@@ -19,10 +19,6 @@ const router = createBrowserRouter([
   {
     path: "/",
     element: <HomeFeedPage />
-  },
-  {
-    path: "/notifications",
-    element: <NotificationsFeedPage />
   },
   {
     path: "/@:handle",
